8d54e9d085a894d53876a9196809c921a959d2da
valoeghese <valoeghesemods@gmail.com>
2021-12-28 00:04:50 +1300

    repackage

--- datafiles/repackage.py
+++ datafiles/repackage.py
@@ -19,18 +19,20 @@ def repackage(tiny, obfregex="", indexFn=lambda x : ord(x)-97):
                 if re.search(r"pkg[0-9]+\/", tokens[2]) is None:
                     packages = tokens[1].split("/")
                     clazz = tokens[2].split("/")[-1]
-                    new_line = "CLASS\t"
-                    new_line += tokens[1] + "\t"
+                    new_line_start = "CLASS\t"
+                    new_line_start += tokens[1] + "\t"
+                    new_intermediary = ""
 
                     # add packages
                     for package in packages[:-1]:
                         if matcher(package):
                             print(package)
-                            new_line += "pkg" + str(indexFn(package)) + "/"
+                            new_intermediary += "pkg" + str(indexFn(package)) + "/"
                         else:
-                            new_line += package + "/" # just the package
-                    new_line += clazz + "\n"
-                    print(new_line)
+                            new_intermediary += package + "/" # just the package
+                    new_intermediary += clazz
+                    print(tokens[2] + " is now " + new_intermediary)
+                    result += new_line_start + new_intermediary + "\n"
                 else:
                     result += line + "\n"
             else:
@@ -42,7 +44,12 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Usage: python repackage.py <intermediaries>")
     else:
+        print("Reading File...")
         with open(sys.argv[1], 'r') as file:
             contents = file.read()
-        
-        repackage(contents, r"[A-z]$")
+
+        print("Repackaging...")
+        contents = repackage(contents, r"[A-z]$")
+        print("Writing...")
+        with open(sys.argv[1], 'w') as file:
+            file.write(contents)
